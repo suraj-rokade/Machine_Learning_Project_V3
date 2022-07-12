98e86fd80992d283170562c1dcf101c4c5c562b7
Suraj <rokadesuraj2001@gmail.com>
2022-07-12 15:45:55 +0530

    done

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/",methods=['GET','POST'])
 def index():
-    return "Starting Machine Learning Project"
+    return "CI CD pipline has bee established"
 
 if __name__ == "__main__":
     app.run(debug=True)
